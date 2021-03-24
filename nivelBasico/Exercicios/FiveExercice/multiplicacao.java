6ab0b92da515aabb1ce9e9e14c63c5ad5081218d
Rafaella.lopes <rafinhacrisdenlavor@gmail.com>
2021-03-24 11:40:38 -0300

    add: exercicios

---
+++ nivelBasico/Exercicios/FiveExercice/multiplicacao.java
@@ -0,0 +1,22 @@
+package nivelBasico.Exercicios.FiveExercice;
+
+import java.util.Scanner;
+
+public class multiplicacao {
+	
+	private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	
+		System.out.println("Digite o primeiro número: ");
+		int primeiroNumero = input.nextInt();
+		
+		System.out.println("Digite o segundo número: ");
+		int segundoNumero = input.nextInt();
+		
+		int calculaMultiplicacao = primeiroNumero * segundoNumero;
+		System.out.println("Resultado: \n"
+		+ primeiroNumero + " X " + segundoNumero + " = " + calculaMultiplicacao);
+	}		
+}
+
---
+++ nivelBasico/Exercicios/SixExercice/operacoes.java
@@ -0,0 +1,47 @@
+package nivelBasico.Exercicios.SixExercice;
+
+import java.util.Scanner;
+
+public class operacoes {
+
+	private static Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+//		Exemplo 1:
+		
+		System.out.print("Insira o primeiro número: ");
+		int primeiroNumero = input.nextInt();
+		
+		System.out.print("Insira o segundo número: ");
+		int segundoNumero = input.nextInt();
+		
+		System.out.println("Resultado: \n");
+		System.out.println(primeiroNumero + " + " + segundoNumero + " = " + (primeiroNumero + segundoNumero));
+		System.out.println(primeiroNumero + " - " + segundoNumero + " = " + (primeiroNumero - segundoNumero));
+		System.out.println(primeiroNumero + " X " + segundoNumero + " = " + (primeiroNumero * segundoNumero));
+		System.out.println(primeiroNumero + " / " + segundoNumero + " = " + (primeiroNumero / segundoNumero));
+		System.out.println(primeiroNumero + " mod " + segundoNumero + " = " + (primeiroNumero % segundoNumero));
+		
+//		Exemplo 2:
+//		Scanner input = new Scanner(System.in);
+//		
+//		System.out.print("Insira o primeiro número: ");
+//		int primeiroNumero = input.nextInt();
+//		
+//		System.out.print("Insira o segundo número: ");
+//		int segundoNumero = input.nextInt();
+//		
+//		int calculoSoma = primeiroNumero + segundoNumero;
+//		int calculoSubtracao = primeiroNumero - segundoNumero;
+//		int calculoMultiplicacao = primeiroNumero * segundoNumero;
+//		int calculoDivisao = primeiroNumero / segundoNumero;
+//		int calculoRestante = primeiroNumero % segundoNumero;
+//		
+//		System.out.println("Resultado: \n");
+//		System.out.println(primeiroNumero + " + " + segundoNumero + " = " + calculoSoma);
+//		System.out.println(primeiroNumero + " - " + segundoNumero + " = " + calculoSubtracao);
+//		System.out.println(primeiroNumero + " X " + segundoNumero + " = " + calculoMultiplicacao);
+//		System.out.println(primeiroNumero + " / " + segundoNumero + " = " + calculoDivisao);
+//		System.out.println(primeiroNumero + " mod " + segundoNumero + " = " + calculoRestante);	
+	}
+}
