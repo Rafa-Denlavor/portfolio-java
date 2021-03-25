06e8d202168198acee5954a71f3aa3399c9b33fa
Rafaella.lopes <rafinhacrisdenlavor@gmail.com>
2021-03-25 11:49:15 -0300

    add: exercice

---
+++ basicLevel/Exercice/ExerciceSixteen/rosto.java
@@ -0,0 +1,14 @@
+package basicLevel.Exercice.ExerciceSixteen;
+
+public class rosto {
+
+    public static void main(String[] args) {
+		
+		System.out.println(" + \"\" \"\"\"+");
+		System.out.println("[|  o o  |]");
+		System.out.println(" |   ^   |");
+		System.out.println(" |  '-'  |");
+		System.out.println(" + ----- +");
+	}
+}
+
