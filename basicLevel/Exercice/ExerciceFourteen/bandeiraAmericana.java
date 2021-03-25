aff01cdc7c71241bfb2a793e79432bafeb14c433
Rafaella.lopes <rafinhacrisdenlavor@gmail.com>
2021-03-25 11:50:04 -0300

    add: new examples

--- basicLevel/Exercice/ExerciceFourteen/bandeiraAmericana.java
+++ basicLevel/Exercice/ExerciceFourteen/bandeiraAmericana.java
@@ -7,16 +7,36 @@ public class bandeiraAmericana {
 //		Exemplo 1:
 		String primeiraOrdem = "* * * * * * ====================================== \n * * * * *  ======================================";
 		String segundaOrdem = "==================================================";
-		
-		for(int i = 0; i < 4; i++) {
+
+		for(int i = 0; i < primeiraOrdem.length(); i++) {
 			System.out.println(primeiraOrdem);
 		}
-		
-		for(int i = 0; i < 6; i++) {
+
+		for(int i = 0; i < segundaOrdem.length(); i++) {
 			System.out.println(segundaOrdem);
 		}
-		
+
 //		Exemplo 2:
+//		String primeiraOrdem = "* * * * * * ====================================== \n * * * * *  ======================================";
+//		String segundaOrdem = "==================================================";
+//		
+//		String desenhoOrdenado[] = new String[9];
+//		
+//		desenhoOrdenado[0] = primeiraOrdem;
+//		desenhoOrdenado[1] = primeiraOrdem;
+//		desenhoOrdenado[2] = primeiraOrdem;
+//		desenhoOrdenado[3] = primeiraOrdem;
+//		desenhoOrdenado[4] = segundaOrdem;
+//		desenhoOrdenado[5] = segundaOrdem;
+//		desenhoOrdenado[6] = segundaOrdem;
+//		desenhoOrdenado[7] = segundaOrdem;
+//		desenhoOrdenado[8] = segundaOrdem;
+//		
+//		for(int i = 0; i < desenhoOrdenado.length; i++) {
+//			System.out.println(desenhoOrdenado[i]);
+//		}		
+
+//		Exemplo 3:
 //		System.out.println("* * * * * * ======================================");
 //		System.out.println(" * * * * *  ======================================");
 //		System.out.println("* * * * * * ======================================");
