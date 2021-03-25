053f26b978fd766aad4ed254b573cab1f697bcfb
Rafaella.lopes <rafinhacrisdenlavor@gmail.com>
2021-03-25 11:56:22 -0300

    add: new examples

--- basicLevel/Exercice/ExerciceSixteen/rosto.java
+++ basicLevel/Exercice/ExerciceSixteen/rosto.java
@@ -3,12 +3,26 @@ package basicLevel.Exercice.ExerciceSixteen;
 public class rosto {
 
     public static void main(String[] args) {
-		
-		System.out.println(" + \"\" \"\"\"+");
-		System.out.println("[|  o o  |]");
-		System.out.println(" |   ^   |");
-		System.out.println(" |  '-'  |");
-		System.out.println(" + ----- +");
-	}
+	
+//		Exemplo 1:
+        String desenhoRosto[] = new String[5];
+        
+        desenhoRosto[0] = " + \"\" \"\"\"+";
+        desenhoRosto[1] = "[|  o o  |]";
+        desenhoRosto[2] = " |   ^   | ";
+        desenhoRosto[3] = " |  '-'  | ";
+        desenhoRosto[4] = " + ----- + ";
+        
+        for(int i = 0; i < desenhoRosto.length; i++) {
+            System.out.println(desenhoRosto[i]);
+        }
+        
+//		Exemplo 2:
+//		System.out.println(" + \"\" \"\"\"+");
+//		System.out.println("[|  o o  |]");
+//		System.out.println(" |   ^   | ");
+//		System.out.println(" |  '-'  | ");
+//		System.out.println(" + ----- + ");
+    }
 }
-
+        
