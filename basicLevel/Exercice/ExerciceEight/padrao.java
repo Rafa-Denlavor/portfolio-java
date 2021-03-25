ba1c10177ff240b24e72d7732aed5462a9bf55ca
Rafaella.lopes <rafinhacrisdenlavor@gmail.com>
2021-03-25 11:59:45 -0300

    add: new examples

--- basicLevel/Exercice/ExerciceEight/padrao.java
+++ basicLevel/Exercice/ExerciceEight/padrao.java
@@ -1,12 +1,24 @@
 package basicLevel.Exercice.ExerciceEight;
 
 public class padrao {
-    	
+
 	public static void main(String[] args) {
+    	
+//		Exemplo 1:
+		String desenhoPadrao[] = new String[4];
+		desenhoPadrao[0] = "   Javva";
+		desenhoPadrao[1] = "   J aavvaa";
+		desenhoPadrao[2] = "JJ aaaaa VV aaaaa";
+		desenhoPadrao[3] = " JJ aa V aa";
 		
-		System.out.println("   Javva");
-		System.out.println("   J aavvaa");
-		System.out.println("JJ aaaaa VV aaaaa");
-		System.out.println(" JJ aa V aa");
+		for(int i = 0; i < desenhoPadrao.length; i++) {
+			System.out.println(desenhoPadrao[i]);
+		}
+
+//		Exemplo 2: 
+//		System.out.println("   Javva");
+//		System.out.println("   J aavvaa");
+//		System.out.println("JJ aaaaa VV aaaaa");
+//		System.out.println(" JJ aa V aa");
 	}
 }
